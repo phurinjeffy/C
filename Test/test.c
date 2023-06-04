784495285e80300d7cfe0486eaa8f692d22f3d8c
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-06-04 09:34:17 +0700

    update

--- Test/test.c
+++ Test/test.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
+  printf("Hello World!\n");
+
+  int myInt = 7;
+  double myDouble = 25.5;
+
+  printf("%i\n", myInt);
+  printf("%lf\n", myDouble);
+
+  for(int i = 0; i < 10; i++){
+    if(i%2){
+      printf("%i\n", i);
+    }
+  }
+  
   return 0;
 }
