605111f53f109eb8d98a1b300727489dedeaec1d
Phurin Vanasrivilai <110296454+phurinjeffy@users.noreply.github.com>
2023-06-17 19:59:18 +0700

    Update ft_putnbr_base.c

--- 42_Piscine/C04/ex04/ft_putnbr_base.c
+++ 42_Piscine/C04/ex04/ft_putnbr_base.c
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(int n)
+#include <unistd.h>
+
+int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -43,10 +45,67 @@ int	check_base(char *base)
 	return (1);
 }
 
+int power(int nb)
+{
+    int e;
+
+    e = 1;
+    while (nb)
+    {
+        e *= 10;
+        nb /= 10;
+    }
+
+    return (e / 10);
+}
+
+void ft_putnbr(int nb)
+{
+    int e;
+    char temp;
+
+    e = power(nb);
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        nb *= -1;
+    }
+    while (nb)
+    {
+        temp = nb / e + '0';
+        write(1, &temp, 1);
+        nb %= e;
+        e /= 10;
+    }
+}
+
+void    decimal(int nbr, char *base)
+{
+    while (nbr)
+    {
+        ft_putnbr(nbr)
+    }
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
 	if (check_base(base) == 1)
 	{
+        if (ft_strlen(base) == 10)
+        {
 
+        }
+        else if (ft_strlen(base) == 2)
+        {
+
+        }
+        else if (ft_strlen(base) == 16)
+        {
+
+        }
+        else if (ft_strlen(base) == 8)
+        {
+            
+        }
 	}
 }
